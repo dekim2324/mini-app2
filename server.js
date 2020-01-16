efd7c5f96ea77c8ee25a82b9a0a4e687ca79d7bb
Dennis Kim <dekim2324@gmail.com>
2020-01-16 00:00:40 -0600

    route for registration set up

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 
+// Init middleware
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('success')
+    res.json({ msg: 'Welcome to Photo Keeper Page!'})
 });
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 3007;
 app.listen(PORT, () => {
